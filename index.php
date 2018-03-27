39f0d65ded2c58ae59dc012a31c011f2e8bd1a2d
Hong Chen <chen1@ualberta.ca>
2018-03-27 07:50:12 -0600

    a little style change

--- index.php
+++ index.php
@@ -1,125 +1,47 @@
 <!DOCTYPE html >
+<html>
 
-  <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-    <title>Using MySQL and PHP with Google Maps</title>
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 80%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 90%;
-        width:80%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-  </head>
+<head>
 
-  <body>
-    <div id="map" class="content"></div>
+<style>
 
-    <script>
-      var customLabel = {
-        restaurant: {
-          label: 'R'
-        },
-        bar: {
-          label: 'B'
-        }
-      };
+body {
+  background-color: lightblue;
+  background-image: url("bus ® Freepik.jpg");
+  background-repeat: no-repeat;
+  background-position: 50% 20%;
+  resize: both;
+}
 
-        function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: new google.maps.LatLng(53.5232, -113.5263),
-          zoom: 10
-        });
-        var infoWindow = new google.maps.InfoWindow;
+h1   {color: blue;}
+p    {color: red;}
+</style>
 
-          // Change this depending on the name of your PHP or XML file
-          downloadUrl('https://storage.googleapis.com/mapsdevsite/json/mapmarkers2.xml', function(data) {
-            var xml = data.responseXML;
-            var markers = xml.documentElement.getElementsByTagName('marker');
-            Array.prototype.forEach.call(markers, function(markerElem) {
-              var id = markerElem.getAttribute('id');
-              var name = markerElem.getAttribute('name');
-              var address = markerElem.getAttribute('address');
-              var type = markerElem.getAttribute('type');
-              var point = new google.maps.LatLng(
-                  parseFloat(markerElem.getAttribute('lat')),
-                  parseFloat(markerElem.getAttribute('lng')));
-
-              var infowincontent = document.createElement('div');
-              var strong = document.createElement('strong');
-              strong.textContent = name
-              infowincontent.appendChild(strong);
-              infowincontent.appendChild(document.createElement('br'));
-
-              var text = document.createElement('text');
-              text.textContent = address
-              infowincontent.appendChild(text);
-              var icon = customLabel[type] || {};
-              var marker = new google.maps.Marker({
-                map: map,
-                position: point,
-                label: icon.label
-              });
-              marker.addListener('click', function() {
-                infoWindow.setContent(infowincontent);
-                infoWindow.open(map, marker);
-              });
-
-              var flightPlanCoordinates = [
-                {lat: 53.5225, lng: -113.6242},
-                {lat: 53.4855, lng: -113.5137},
-                {lat: 53.5232, lng: -113.5263}
-              ];
-              var flightPath = new google.maps.Polyline({
-                path: flightPlanCoordinates,
-                geodesic: true,
-                strokeColor: '#FF0000',
-                strokeOpacity: 1.0,
-                strokeWeight: 2
-              });
-
-              flightPath.setMap(map);
-
-
-            });
-          });
-        }
+</head>
 
 
 
-      function downloadUrl(url, callback) {
-        var request = window.ActiveXObject ?
-            new ActiveXObject('Microsoft.XMLHTTP') :
-            new XMLHttpRequest;
+<body>
+  <style>
 
-        request.onreadystatechange = function() {
-          if (request.readyState == 4) {
-            request.onreadystatechange = doNothing;
-            callback(request, request.status);
-          }
-        };
+  a {
+      position: relative;
+      left: 500px;
+      top: 200px;
+  }
 
-        request.open('GET', url, true);
-        request.send(null);
-      }
+  </style>
 
-      function doNothing() {}
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDR4v_523VmlE7u7_6GNw7ODu9Ku9_Ckws&callback=initMap">
-    </script>
+  <a href="/map.php">
+  <input type="button" value="Congestion Condition Map" />
+  </a>
 
-    <a href="/pb.php">
-       <input type="button" value="Visit Real Time page" />
-    </a>
 
-  </body>
+
+  <a href="/pb.php">
+  <input type="button" value="Visit Real Time page" />
+  </a>
+
+</body>
+
 </html>
