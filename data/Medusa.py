31dd9de2a7e09cc4e16a301f7cc81e23eee4c128
Peter <atrazhev@ualberta.ca>
2018-05-04 15:01:49 -0600

    final detection script :P

--- data/Medusa.py
+++ data/Medusa.py
@@ -9,17 +9,9 @@ import shlex
 # Camera direction
 
 # TODO:  run darknet on it -> this outputs the number of classes that have been
-cmd = ['awk', 'length($0) > 5']
-# if passing a single string, either shell must be true or else the string
-# must simply name the program to be extecuted without any arguments.
-input = 'foo\nfoofoo\n'.encode('utf-8')
-# This actually runs the command in the command line
 
-result = sp.run(cmd, stdout=sp.PIPE, input=input)
-# decode the output
-result.stdout.decode('utf-8')
-
-# Begine the hackathon code. 
+# Begine the hackathon code: these are the commnads needed to run darknet from
+#python
 args = './darknet detect cfg/yolov3.cfg yolov3.weights data/dog.jpg'
 cwd ='/mnt/c/Users/Peter/source/repos/YOLOv2/darknet'
 result_bytes = sp.check_output([shlex.split(args)], cwd = cwd)
@@ -28,32 +20,27 @@ results_string = result_bytes.decode('utf-8')
 parsed_string = results_string.split('\n')
 #this outputs an array
 #The amount of classes that are outputed is len(parsed_string)-2
+for i in parsed_string:
+    if parsed_string[i] = 'car':
+        car_counter += 1
+    else
+    return
 
-
-
-
-# TODO:  may need to parse the output based on the ouput of the subprocess
-##
-##
-# TODO: count the number of classes
-counted_values = parser.parse
-
-num_classes = counted_values
-# TODO: based on the number of classes:
-# low traffic = 3 cars detected
-# Medium traffic = 6 cars
-# high traffic = more than 6 :P
-
-
-if counted_values <= 3:
+print("The number of cars detected in the image is" + string(car_counter))
+# Test level 1
+if car_counter <= 3:
     traffic_val = "Low"
     color = "Green"
-elif counted_values <= 6:
+elif car_counter <= 8:
     traffic_val = "Medium"
     color = "Yellow"
 else:
     traffic_val = "High"
     color = "Red"
+
+print("The traffic volume is "+traffic_val)
+
+
 
 
 # TODO: now you can color the map
--- data/test/car_detection.py
+++ data/test/car_detection.py
@@ -1,10 +1,15 @@
 import subprocess as sp
 import shlex
+import csv
+import datetime
+import io
 
-args = './darknet detect cfg/yolov3.cfg yolov3.weights data/Accident.JPG -thresh 0.1'
+#args = './darknet detect cfg/yolov3.cfg yolov3.weights data/capilano_test.PNG -thresh 0.1'
+# args = './darknet detect cfg/yolov3.cfg yolov3.weights data/Accident.JPG -thresh 0.1'
+ args = './darknet detect cfg/yolov3.cfg yolov3.weights data/cars.jpg   -thresh 0.1'
 cwd ='/mnt/c/Users/Peter/source/repos/YOLOv2/darknet'
 result_bytes = sp.check_output(shlex.split(args), cwd = cwd)
-#this should run the darknet program
+#this runs the darknet program:
 results_string = result_bytes.decode('utf-8')
 parsed_string = results_string.split('\n')
 #this outputs an array
@@ -15,19 +20,35 @@ parsed_string = results_string.split('\n')
 #    else:
 #        break
 # apparently this will do the same thing:
-car_counter = parsed_string.count('car: %_dontcare_dontcare')
+#car_counter = parsed_string.count().find()
 # we need define some dont care values in the
+car_counter = 0
+for x in enumerate(parsed_string):
+    car_counter += 1
 
-print(car_counter)
+print("The nubmer of detected cars is "+str(car_counter))
 # Test level 1
-if car_counter <= 3:
+if car_counter <= 10:
     traffic_val = "Low"
     color = "Green"
-elif car_counter <= 8:
+elif car_counter <= 20:
     traffic_val = "Medium"
     color = "Yellow"
-else:
+elif car_counter <=30:
     traffic_val = "High"
     color = "Red"
+else:
+    traffic_val = "Accident"
+    color = "Red"
 
-print("The traffic volume is "+traffic_val)
+if traffic_val == "Accident":
+    print("There is a possible " + traffic_val)
+else:
+    print("The traffic volume is "+traffic_val)
+
+time = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+with open('test.csv', 'a', encoding='utf-8', newline='') as csvfile:
+    traffic_writer = csv.writer(csvfile,delimiter=' ',
+    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    traffic_writer.writerow([time, str(car_counter), str(traffic_val)])
+    csvfile.close()
